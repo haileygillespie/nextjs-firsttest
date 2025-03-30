f5d8aeb07147c26d3ec5f368faf2ad4111f72882
Hailey Gillespie <hailey0101gillespie@gmail.com>
2025-03-30 14:53:59 -0500

    6

--- app/page.tsx
+++ app/page.tsx
@@ -4,19 +4,20 @@ import Nav from "./components/nav";
 export default function Home() {
   return (
     <div className="center">
-      <h1>TEST</h1>
-      <p>hi</p>
-      <Image
-          className=""
-          src="/cat.png"
-          alt="cat"
-          width={200}
-          height={200}
-          priority
-        />
-        <Nav>
-          <p>a child element</p>
-        </Nav>
+      <main>
+        <h1>TEST</h1>
+        <Image
+            className="center"
+            src="/cat.png"
+            alt="cat"
+            width={200}
+            height={200}
+            priority
+          />
+          <Nav>
+            <p>a child element</p>
+          </Nav>
+      </main>
     </div>
   );
 }
